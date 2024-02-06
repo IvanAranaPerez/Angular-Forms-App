982a7150423d51adf46b6ce57c5dd0e00fb7820d
IvanAranaPerez <i.arana@dappertechnologies.com>
2024-02-06 13:04:01 -0500

    Integracion de Swtiches

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: ``
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+
+
+  public myForm: FormGroup = this.fb.group({
+    gender: [ 'M', Validators.required ],
+    wantNotifications: [ true, Validators.required ],
+    termsAndConditions: [ false, Validators.requiredTrue ],
+  });
+
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  }
+
+  ngOnInit(): void {
+    this.myForm.reset( this.person );
+  }
+
+  constructor( private fb: FormBuilder ) {}
+
+  isValidSwitch( switchfield: string ): boolean | null {
+    return this.myForm.controls[switchfield].errors
+    && this.myForm.controls[switchfield].touched;
+  }
+
+  onSave() {
+    if (this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
+    console.log(this.myForm.value);
+    console.log(this.person);
+  }
 
 }
